cc229d6f81249d6b81cc73420f3d371439fb3577
RHNX <rhnxofficial@gmail.com>
2025-12-31 18:05:37 +0700

    Jzjs

--- plugins/Storage/addvideo.js
+++ plugins/Storage/addvideo.js
@@ -1,6 +1,6 @@
 "use strict";
 import fs from "fs";
-import { uploadToGithub } from "../../lib/uploader.js";
+import { uploadToRHNX } from "../../lib/uploader.js";
 
 export default {
   name: "addvideo",
@@ -26,7 +26,9 @@ export default {
     try {
       const filePath = await m.quoted.downloadAndSave();
       const buffer = fs.readFileSync(filePath);
-      const urlVideo = await uploadToGithub(buffer);
+      const diUplod = await uploadToRHNX(buffer,'permanent');
+      const urlVideo = diUplod.url;
+      
       videoData.push({
         name,
         urlVideo
