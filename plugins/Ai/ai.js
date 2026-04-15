ed9c42ae8323887fd2980226ded1d3e7d783bab4
RHNX <rhnxofficial@gmail.com>
2026-04-15 20:09:10 +0700

    Update AI plugin to use new alias and API endpoint

--- plugins/Ai/ai.js
+++ plugins/Ai/ai.js
@@ -1,12 +1,17 @@
+"use strict";
+
 import axios from "axios";
+import { sendMixedMessage } from "../../lib/sendMixedMessage.js";
 
 export default {
   name: "ai",
-  alias: ["gemini", "ask"],
+  alias: ["rynex", "ask"],
   description: "Chat Ai",
   access: { group: false },
+
   run: async (m, { conn, args, prefix, command }) => {
     let inputText = args.join(" ");
+
     if (!inputText && m.quoted && m.quoted.text) {
       inputText = m.quoted.text;
     }
@@ -15,7 +20,7 @@ export default {
       return m.reply(
         `Contoh penggunaan:\n${prefix + command} Halo apa kabar\natau reply pesan lalu ketik:\n${prefix + command}`
       );
-
+m.react('✳️')
     const defaultPrompt = `
 Kamu adalah AI pintar. 
 Jika kamu bingung atau tidak yakin, jangan menyerah—coba lagi sampai menemukan jawaban yang paling masuk akal.
@@ -23,17 +28,21 @@ Jangan bilang "saya tidak tahu", cobalah memberikan jawaban terbaik.
 `;
 
     try {
-      const url = `${api.rhnx}/api/ai?text=${encodeURIComponent(
+      const url = `${api.rhnx}/api/ai/rynexchat?text=${encodeURIComponent(
         inputText
       )}&prompt=${encodeURIComponent(defaultPrompt)}&key=${key.rhnx}`;
 
       const res = await axios.get(url);
       const data = res.data;
 
-      if (!data.status || !data.data?.result) return m.reply("Gagal mendapatkan jawaban dari AI.");
+      if (!data.status || !data.data?.result) {
+        return m.reply("Gagal mendapatkan jawaban dari AI.");
+      }
 
       let hasil = data.data.result;
-      await conn.sendMessage(m.chat, { text: hasil }, { quoted: m });
+        
+      await sendMixedMessage(conn, m, hasil);
+
     } catch (err) {
       console.log("Error AI:", err);
       return m.reply("Terjadi kesalahan saat menghubungi AI.");
